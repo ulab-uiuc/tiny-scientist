316b65969bebff811825b50b50d8b055fa483622
4R5T <maxmeetsli@gmail.com>
2025-04-10 20:53:03 -0500

    feat(thinker, demo_deepseek, coder): fix bug in thinker (returning wrong idea), and use v3

--- tiny_scientist/coder.py
+++ tiny_scientist/coder.py
@@ -45,7 +45,7 @@ class Coder:
             yes=True, chat_history_file=chat_history or f"{self.base_dir}/aider.txt"
         )
 
-        if model == "deepseek-coder-v2-0724" or model == "deepseek-reasoner":
+        if model == "deepseek-coder-v2-0724" or model == "deepseek-chat":
             main_model = Model("deepseek/deepseek-coder")
         elif model == "llama3.1-405b":
             main_model = Model("openrouter/meta-llama/llama-3.1-405b-instruct")
