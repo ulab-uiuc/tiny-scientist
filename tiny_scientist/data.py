7297a53db388d6e3fb1468f41120610b99ab3ff1
Ziheng-Qi <zihengq2@illinois.edu>
2025-07-02 21:02:38 -0500

    fix: mypy error

--- tiny_scientist/data.py
+++ tiny_scientist/data.py
@@ -58,6 +58,8 @@ class ThinkerPrompt(BaseModel):
     idea_reflection_prompt: str
     novelty_prompt: str
     experiment_plan_prompt: str
+    ethical_defense_system_prompt: str
+    ethical_defense_prompt: str
 
 
 class DrawerPrompt(BaseModel):
